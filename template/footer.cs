106f42f6f001bb2e031fac98668494be9ad21b1e
Lars Kruse <ezmlm-web@sumpfralle.de>
2005-12-16 23:43:11 +0000

    toolforge link: "_blank"-target removed some old stuff

--- template/footer.cs
+++ template/footer.cs
@@ -1,7 +1,7 @@
 
 <table border="0" cellpadding="5" cellspacing="5" align="center" width="99%">
 <tr><td bgcolor="#e0e0ff"><font size="-1">
-<a href="https://systemausfall.org/toolforge/ezmlm-web" target="_blank">ezmlm-web</a> (v2.3) A web interface to <a href="http://www.ezmlm.org/" target="_blank">ezmlm</a></font>
+<a href="https://systemausfall.org/toolforge/ezmlm-web">ezmlm-web</a> (v2.3) A web interface to <a href="http://www.ezmlm.org/" target="_blank">ezmlm</a></font>
 </td></tr></table>
 
 </body>
