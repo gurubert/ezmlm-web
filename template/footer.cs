c4ae50be9e8c8d66887603cda8f4bcbe27262f52
Lars Kruse <ezmlm-web@sumpfralle.de>
2005-12-22 07:43:31 +0000

    removed deprecated files updated version information

--- template/footer.cs
+++ template/footer.cs
@@ -1,6 +1,6 @@
 
 	<div id="footer">
-		<a href="https://systemausfall.org/toolforge/ezmlm-web">ezmlm-web</a> (v2.3.dev) - <?cs var:html_escape(Lang.Misc.FooterText) ?> <a href="http://www.ezmlm.org/" target="_blank">ezmlm</a>
+		<a href="https://systemausfall.org/toolforge/ezmlm-web">ezmlm-web</a> (v3.0) - <?cs var:html_escape(Lang.Misc.FooterText) ?> <a href="http://www.ezmlm.org/" target="_blank">ezmlm</a>
 		<br />
 	</div>
 
