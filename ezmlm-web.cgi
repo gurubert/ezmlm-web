4c5b241dcc52cb896c3e28c2f26e3f53a610594f
Lars Kruse <ezmlm-web@sumpfralle.de>
2006-01-07 01:10:00 +0000

    infrastructure for building debian packages

--- ezmlm-web.cgi
+++ ezmlm-web.cgi
@@ -1,37 +1,6 @@
 #!/usr/bin/perl 
 #===========================================================================
-# ezmlm-web.cgi - version 2.3 - 10/06/02005
-#
-# Copyright (C) 1999/2000, Guy Antony Halse, All Rights Reserved.
-# Please send bug reports and comments to guy-ezmlm@rucus.ru.ac.za
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met: 
-#
-# Redistributions of source code must retain the above copyright notice,
-# this list of conditions and the following disclaimer.
-#
-# Redistributions in binary form must reproduce the above copyright notice,
-# this list of conditions and the following disclaimer in the documentation
-# and/or other materials provided with the distribution.
-#
-# Neither name Guy Antony Halse nor the names of any contributors
-# may be used to endorse or promote products derived from this software
-# without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS
-# IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-# THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
+# ezmlm-web.cgi - version 3.1
 # ==========================================================================
 # All user configuration happens in the config file ``ezmlmwebrc''
 # POD documentation is at the end of this file
@@ -93,8 +62,10 @@ if(defined($opt_C)) {
    require "$HOME_DIR/.ezmlmwebrc"; # User
 } elsif(-e "./ezmlmwebrc") {
    require "./ezmlmwebrc"; # Install
+} elsif(-e "/etc/ezmlm-web/ezmlmwebrc") {
+   require "/etc/ezmlm-web/ezmlmwebrc"; # System (new style)
 } elsif(-e "/etc/ezmlm/ezmlmwebrc") {
-   require "/etc/ezmlm/ezmlmwebrc"; # System
+   require "/etc/ezmlm/ezmlmwebrc"; # System (old style)
 } else {
    &fatal_error("Unable to read config file");
 }
---
+++ scripts/debian.sh
@@ -0,0 +1,76 @@
+#!/bin/sh
+#
+#  Copyright (c) 02005 Lars Kruse <devel@sumpfralle.de>
+#
+#  License:  This script is distributed under the terms of 
+#            the BSD license
+#
+# build a debian package
+#
+
+set -ue
+
+######### some settings ###########
+ROOT_DIR=$(dirname "$0")/..
+ROOT_DIR=$(cd "$ROOT_DIR"; pwd)
+
+BUILD_DIR=/tmp/ezmlm-web-build-$$
+
+PACKAGE_DIR=$ROOT_DIR/../tags/packages
+[ ! -e "$PACKAGE_DIR" ] && PACKAGE_DIR=$ROOT_DIR/../packages
+[ ! -e "$PACKAGE_DIR" ] && echo "package dir not found" >&2 && exit 1
+
+############# do it ###############
+
+[ -e "$BUILD_DIR" ] && rm -rf "$BUILD_DIR"
+
+ACTION=build
+[ $# -gt 0 ] && ACTION=$1 && shift
+
+case "$ACTION" in
+	build )
+		mkdir -p "$BUILD_DIR/usr/share/ezmlm-web"
+		mkdir -p "$BUILD_DIR/usr/lib"
+		mkdir -p "$BUILD_DIR/usr/bin"
+		mkdir -p "$BUILD_DIR/usr/share/man/man1"
+		mkdir -p "$BUILD_DIR/usr/share/doc/ezmlm-web/examples"
+		mkdir -p "$BUILD_DIR/etc/ezmlm-web"
+		mkdir -p "$BUILD_DIR/var/www"
+		svn export "$ROOT_DIR/template" "$BUILD_DIR/usr/share/ezmlm-web/template" >/dev/null
+		svn export "$ROOT_DIR/css" "$BUILD_DIR/usr/share/ezmlm-web/css" >/dev/null
+		ln -s /usr/share/ezmlm-web/css/default.css "$BUILD_DIR/var/www/ezmlm-web.css"
+		svn export "$ROOT_DIR/lang" "$BUILD_DIR/usr/share/ezmlm-web/lang" >/dev/null
+		cp "$ROOT_DIR/ezmlm-web.cgi" "$BUILD_DIR/usr/lib"
+		cp "$ROOT_DIR/debian-related/index.c" "$BUILD_DIR/usr/share/ezmlm-web"
+		cp "$ROOT_DIR/htaccess.sample" "$BUILD_DIR/usr/share/doc/ezmlm-web/examples"
+		cp "$ROOT_DIR/webusers.sample" "$BUILD_DIR/usr/share/doc/ezmlm-web/examples"
+		sed 's#/usr/local/#/usr/#g' "$ROOT_DIR/ezmlmwebrc" | tee "$BUILD_DIR/usr/share/doc/ezmlm-web/examples/ezmlmwebrc" >"$BUILD_DIR/etc/ezmlm-web/ezmlmwebrc"
+		cp "$ROOT_DIR/README" "$BUILD_DIR/usr/share/doc/ezmlm-web"
+		cp "$ROOT_DIR/TODO" "$BUILD_DIR/usr/share/doc/ezmlm-web"
+		cp "$ROOT_DIR/UPGRADING" "$BUILD_DIR/usr/share/doc/ezmlm-web"
+		cp "$ROOT_DIR/copyright" "$BUILD_DIR/usr/share/doc/ezmlm-web"
+		cp "$ROOT_DIR/debian-related/README.Debian" "$BUILD_DIR/usr/share/doc/ezmlm-web"
+		cp "$ROOT_DIR/debian-related/ezmlm-web-make-suid" "$BUILD_DIR/usr/bin"
+		gzip --best -c "$ROOT_DIR/changelog" \
+			>"$BUILD_DIR/usr/share/doc/ezmlm-web/changelog.gz"
+		gzip --best -c "$ROOT_DIR/debian-related/changelog.Debian" \
+			>"$BUILD_DIR/usr/share/doc/ezmlm-web/changelog.Debian.gz"
+		gzip --best -c "$ROOT_DIR/debian-related/man/ezmlm-web-make-suid.1" \
+			>"$BUILD_DIR/usr/share/man/man1/ezmlm-web-make-suid.1.gz"
+		svn export "$ROOT_DIR/debian-related/DEBIAN" "$BUILD_DIR/DEBIAN" >/dev/null
+		fakeroot dpkg-deb --build "$BUILD_DIR" "$PACKAGE_DIR"
+		rm -rf "$BUILD_DIR"
+		;;
+	check )
+		PACKAGE_FILE=$(find "$PACKAGE_DIR" -type f -name "ezmlm-web*" | grep "\.deb$" | sort -n | tail -1)
+		if [ -z "$PACKAGE_FILE" ]
+			then	echo "no debian package found in $PACKAGE_DIR"
+			else	lintian "$PACKAGE_FILE"
+		  fi
+		;;
+	* )
+		echo "Syntax: $(basename $0) [ build | check | help ]"
+		echo
+		;;
+  esac
+
