8eb2e015ce9d9275ef7f4a9a76bb7868ef9231e2
Lars Kruse <ezmlm-web@sumpfralle.de>
2008-09-27 23:54:34 +0000

    update template according to new web interface code

--- template/config_options/create_listname.cs
+++ template/config_options/create_listname.cs
@@ -1,2 +1,2 @@
 <label for="listname"><?cs var:html_escape(Lang.Misc.ListName) ?>:</label>
-	<input type="text" name="list" id="listname" size="25">
+	<input type="text" name="new_list" id="listname" size="25">
